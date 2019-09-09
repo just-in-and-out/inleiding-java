13b519fe6d3d419368882b3330b40a922ae20792
just-in-and-out <jdelaat@roc-dev.com>
2019-09-09 09:35:03 +0200

    t/m H2 opdracht 1

---
+++ src/h02/opdracht1.java
@@ -0,0 +1,17 @@
+package h02;
+
+        import java.applet.Applet;
+        import java.awt.*;
+
+public class opdracht1 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("justin!!", 50, 60 );
+    }
+
+}
+}
