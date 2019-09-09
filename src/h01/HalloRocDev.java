8f5958eb806d561b7e81ca9fa70846ee65ec59c3
just-in-and-out <jdelaat@roc-dev.com>
2019-09-09 09:33:25 +0200

    opdracht 1 H02 opdracht1

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -1,11 +1,11 @@
 package h01;
+//v1:
+//v1:
+//v1:
 
-/**
- * Created by piet on 31-08-16.
- */
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 8A!");
+        System.out.println("it's a me. mario");
     }
 }
