c9aa43f80ce1bf3dd177f699996d170ab264f324
just-in-and-out <jdelaat@roc-dev.com>
2019-09-10 14:07:04 +0200

    h07 opdracht 1

---
+++ src/h07/opdracht1.java
@@ -0,0 +1,10 @@
+package h07;
+
+public class opdracht1 {
+
+    //v1: java.lang.Object -->java.awt.Component -->java.awt.Container -->java.awt.Panel -->java.applet.Applet
+    //v2: java.applet.Applet.AccessibleApplet
+    //v3: functie? (niet kunnen vinden)
+
+
+}
