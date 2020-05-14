3c87bb485ab91725e40d018814ad0531ff805273
just-in-and-out <54847960+just-in-and-out@users.noreply.github.com>
2020-05-14 12:43:55 +0200

    Update opdracht1.java

--- src/h12/opdracht1.java
+++ src/h12/opdracht1.java
@@ -3,28 +3,24 @@ package h12;
 import java.applet.Applet;
 import java.awt.*;
 
-public class opdracht1 extends Applet {
-    double[] salaris;
-    double total;
+public class Opdracht2 extends Applet {
+
+    Button[] buttons;
 
     public void init() {
-        setSize(600,400);
-        salaris = new double[9];
 
-        for (int teller = 0; teller < 9; teller++){
+        buttons = new Button[25];
 
-            salaris[teller] = 25 * (teller + 1);
-
-            total = salaris[teller] / 2;
+        for (int i = 0; i < buttons.length; i++) {
+            Button button = new Button();
+            buttons[i] = button;
         }
-
-
+        for (int i = 0; i < buttons.length; i++) {
+            add(buttons[i]);
+        }
     }
 
     public void paint(Graphics g) {
-        for (int teller = 0; teller < salaris.length; teller ++) {
-            g.drawString(" " + salaris[teller], 100, 20 + 20 * teller);
-            g.drawString(" " + total, 200, 50);
-        }
+
     }
 }
