e67ba1d8b2138f660cde120cdcf845cee1f753d6
just-in-and-out <54847960+just-in-and-out@users.noreply.github.com>
2020-05-27 10:57:08 +0200

    Update opdracht1.java

--- src/h12/opdracht1.java
+++ src/h12/opdracht1.java
@@ -3,24 +3,24 @@ package h12;
 import java.applet.Applet;
 import java.awt.*;
 
-public class Opdracht2 extends Applet {
+public class opdracht1 extends Applet {
 
-    Button[] buttons;
+    double[] getallen = {1,2,3,4,5,6,7,8,9,10};
+    double totaal, gemiddelde;
 
-    public void init() {
+    public void init(){
 
-        buttons = new Button[25];
-
-        for (int i = 0; i < buttons.length; i++) {
-            Button button = new Button();
-            buttons[i] = button;
+        totaal = 0;
+        for (int i = 0; i < getallen.length; i++) {
+            totaal += getallen[i];
         }
-        for (int i = 0; i < buttons.length; i++) {
-            add(buttons[i]);
-        }
+        System.out.println(totaal);
+        gemiddelde = totaal / getallen.length;
     }
-
     public void paint(Graphics g) {
-
+        for (int i = 0; i < getallen.length; i++) {
+            g.drawString(" "+ getallen[i],50,25*i+10);
+            g.drawString(" " + gemiddelde,100,110);
+        }
     }
 }
