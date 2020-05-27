9310b517948247b723bdcac5d1e8eac0f77c8a83
just-in-and-out <54847960+just-in-and-out@users.noreply.github.com>
2020-05-27 10:57:41 +0200

    Update opdracht3.java

--- src/h12/opdracht3.java
+++ src/h12/opdracht3.java
@@ -1,4 +1,3 @@
-
 package h12;
 
 import java.applet.Applet;
@@ -12,47 +11,48 @@ public class opdracht3 extends Applet {
     public TextField[] textFields;
 
     private int teller = 0;
-    int middle = 0;
-    int number;
-    public String converter;
+    public String converter[];
     int numbers[];
     int counter = 0;
 
     public void init() {
 
+        converter = new String[5];
+        numbers = new int[5];
+
         Button OK = new Button("OK");
         OK.addActionListener(new Oklisetener());
+        add(OK);
 
         textFields = new TextField[5];
 
-        add(OK);
-        converter = " ";
-
         while (teller < 5){
+
+
 
             textFields[teller] = new TextField();
             add(textFields[teller]);
             teller++;
-
         }
     }
 
     public void paint(Graphics g) {
 
-        g.drawString(" " + middle,20,20);
-
-        g.drawString(" "+ numbers[counter],50,50);
 
     }
 
     class Oklisetener implements ActionListener{
         public void actionPerformed(ActionEvent e) {
             for (counter = 0; counter < 5;counter++){
-                converter = textFields[counter].getText();
-                numbers[counter] = Integer.parseInt(converter);
-                repaint();
+                converter[counter] = textFields[counter].getText();
+                numbers[counter] = Integer.parseInt(converter[counter]);
             }
             Arrays.sort(numbers);
+            for (int x = 0;x < 5; x++){
+                converter[x] = String.valueOf(numbers[x]);
+                textFields[x].setText(converter[x]);
+                repaint();
+            }
         }
     }
 }
